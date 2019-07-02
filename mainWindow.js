63dfe15d6955506867e1d257ed9f2f89f659845d
Hugo-AOYAGI <aoyagihugo@gmail.com>
2019-07-02 21:32:09 +0200

    rebuilt the JS structure for the mainWindow.js to add jquery.

--- mainWindow.js
+++ mainWindow.js
@@ -1,177 +1,56 @@
+  
+// Wait for window to load
+$(document).ready(() => { 
+  /* ===Creating the day Selection bar=== */
 
 
-/*DAY SELECTION BAR*/
-
-// Setting up all the buttons for the day selection bar
-const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
-let body = document.querySelector("body");
-let buttons = [];
-// Getting the template
-let selection_bar = document.querySelector(".day-selection-bar");
-let template = document.querySelector(".template.__day-button");
-for(day of days) {
-  copy = template.cloneNode(true);
-  // Changing the template with the right text
-  copy.innerHTML = copy.innerHTML.replace("Day Name", day);
-  copy.innerHTML = copy.innerHTML.replace("X", "0");
-  copy.setAttribute("class", "__day-button")
-  buttons.push(copy);
-  // Displaying all the buttons
-  selection_bar.appendChild(copy);
-}
-
-template.style.display = "none";
+  /* ===Adding footer events listeners=== */
 
 
-// Manages the navigation bar button
-let nav_btn = document.querySelector(".nav-btn");
-nav_btn.addEventListener("click", () => {toggleNavBar();});
 
-// Allows for the navigation bar to appear and disappear
-toggleNavBar = () => {
-  if (selection_bar.style.display == "none" || selection_bar.style.display == "" ) {
-    body.style.gridTemplate = '"head head timer" 2fr "nav main main" 12fr "nav foot foot" 1fr / 1fr 3.5fr 1.5fr';
-    selection_bar.style.display = "flex";
-    nav_btn.style.transform = "rotateZ(90deg)";
-  } else {
-    body.style.gridTemplate = '"head head timer" 2fr "main main main" 12fr "foot foot foot" 1fr / 1fr 3.5fr 1.5fr';
-    selection_bar.style.display = "none";
-    nav_btn.style.transform = "rotateZ(0deg)";
-  }
-}
-
-/*HEADER*/
-
-//Displaying the date on the page header
-let date_span = document.querySelector(".__date");
-let time_span = document.querySelector(".__time");
-
-//Adds a zero before a date or time if it is lesser than 10
-leadingZero = (n) => {
-  return n < 10 ? "0"+n : n;
-}
-
-//Reloads the date every second
-reloadDate = () => {
-  let d = new Date();
-  // Formats the date with leading zeros and dashes
-  let txt_date = `${leadingZero(d.getDate())} /
-                  ${leadingZero(d.getMonth()+1)} /
-                  ${leadingZero(d.getFullYear())}`;
-  // Formats the time with leading zeros and semi-colons
-  let txt_time = `${leadingZero(d.getHours())} :
-                  ${leadingZero(d.getMinutes())} :
-                  ${leadingZero(d.getSeconds())}`;
-  date_span.innerHTML = txt_date;
-  time_span.innerHTML = txt_time;
-}
-reloadDate();
-let t = setInterval(reloadDate, 1000);
-
-/*TASK CARDS*/
-
-//Manages the sidebar buttons on each task
-let tab_buttons = document.getElementsByClassName("__tab-main");
-let sidebars = document.getElementsByClassName("__side-bar");
-let titles = document.querySelectorAll(".task .__title");
-
-// Add event listeners to each tab
-for(let i = 0; i < tab_buttons.length; i++) {
-  tab_buttons[i].addEventListener("click", () => {toggleSideBar(i);});
-}
-
-// Displays the transition when the button is pressed
-function toggleSideBar(i) {
-  // Checks whether the sidebar is already shown or not
-  if (tab_buttons[i].innerHTML == "〉") {
-    sidebars[i].style.width = "15%";
-    tab_buttons[i].style.left = "15%";
-    tab_buttons[i].innerHTML = "〈";
-    titles[i].style.marginLeft = "15%";
-  } else {
-    sidebars[i].style.width = "0";
-    tab_buttons[i].style.left = "0%";
-    tab_buttons[i].innerHTML = "〉";
-    titles[i].style.marginLeft = "0";
-  }
-}
-
-// Manages the more-info button on each task which displays the full task card
-let more_info_buttons = document.getElementsByClassName("__more-button");
-let tasks = document.getElementsByClassName("task");
-let time_divs = document.getElementsByClassName("__start-time");
-let task_cards = document.getElementsByClassName("task-card");
+  /* ===Displaying the time onto the header=== */
 
 
-// Add event listeners to each button
-for(let i = 0; i < tasks.length; i++) {
-  more_info_buttons[i].addEventListener("click", () => {toggleTaskCard(i);});
-}
 
-// Displays the transition when the button is pressed
-function toggleTaskCard(i) {
-  // Checks whether the card is already shown or not
-  if (tasks[i].style.height == "20%" || tasks[i].style.height == "") {
-    if (tab_buttons[i].innerHTML != "〉") {
-      toggleSideBar(i);
-    }
-    tasks[i].style.height = "10%";
-    more_info_buttons[i].style.transform = "rotateZ(0deg)";
-    time_divs[i].style.opacity = "0";
-    tab_buttons[i].style.left = "-1em";
-    setTimeout(() => {task_cards[0].style.transform = "translateX(0)";}, 450);
-    // Hides all the other tasks
-    for(let j = 0; j < tasks.length; j++) {
-      if (j != i){
-        setTimeout(() => {
-          tasks[j].style.display = "none";
-        }, 300);
-      }
-      tasks[j].style.left = "-100%";
-      setTimeout(() => {
-        tasks[i].style.left = "0";
-      }, 600);
-      
-    }
-  } else {
-    tasks[i].style.height = "20%";
-    more_info_buttons[i].style.transform = "rotateZ(45deg)";
-    time_divs[i].style.opacity = "1";
-    tab_buttons[i].style.left = "0";
-    tasks[i].style.left = "-100%";
-    task_cards[0].style.transform = "translateX(100%)";
-    // Unhides all the other tasks
-    for(let j = 0; j < tasks.length; j++) {
-      if (j != i){
-        setTimeout(() => {
-          tasks[j].style.display = "block";
-        }, 300);
-      }
-      setTimeout(() => {
-        tasks[j].style.left = "0";
-      }, 600);
-    }
-  }
-}
+  /* ===Managing the next-task element=== */
 
-/*DAY CARDS*/
-
-// Makes 7 copy of the template for each day of the week
-let day_cards = [];
-let template_day_card = document.querySelector(".template.day-card");
-let day_cards_container = document.querySelector(".day-card-container");
-
-for(day of days) {
-  copy = template_day_card.cloneNode(true);
-  // Changing the template with the right text
-  copy.innerHTML = copy.innerHTML.replace("[DAY-NAME]", day);
-  copy.setAttribute("class", "day-card")
-  copy.style.display = "grid";
-  day_cards.push(copy);
-  day_cards_container.appendChild(copy);
-}
-
-template_day_card.style.display = "none";
+  // Display the time left until the next task/event
 
 
+  // Add event listener to the next-task element thats redirects to the corresponding task card
+
+
+  /* ===Managing the day card=== */
+
+  // Load the tasks of the day card
+
+
+  // Load the schedule of the day card
+
+
+  /* ===Managing the task card=== */
+
+  // Add event listeners to go to task card with the more-info button
+
+
+  // Add event listeners to the tab and sidebar buttons
+
+
+  // Remove option for the task
+
+
+  // Edit option for the task
+
+
+  // Check option for the task
+
+
+  /* ===New task feature=== */
+
+  // Create new html window (addTask.html) from the main.js
+
+
+  // Reload the tasks so that the new task appears
+
+
+}); 
